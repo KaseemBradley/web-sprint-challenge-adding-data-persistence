8067a11249bc734ff36cb502587a7501a3a85218
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-06-11 12:17:09 -0400

    more tests

--- api/project/router.js
+++ api/project/router.js
@@ -3,14 +3,21 @@ const express = require("express");
 const router = express.Router();
 const Projects = require("./model");
 
-router.get("/", async (req, res, next) => {
-  try {
-    const project = await Projects.find();
-    res.json(project);
-    console.log(project);
-  } catch (error) {
-    next(error);
-  }
+router.get("/", (req, res) => {
+  Projects.find()
+    .then((data) => {
+      data.map((project) => {
+        project.project_completed
+          ? (project.project_completed = true)
+          : (project.project_completed = false);
+        return project;
+      });
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err.message);
+    });
 });
 
 module.exports = router;
