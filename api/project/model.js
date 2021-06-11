3b7987375adb91a95c79abdb42f18ca8b9a29329
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-06-11 10:41:41 -0400

    wrote project models

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,22 @@
 // build your `Project` model here
+const db = require("../../data/dbConfig");
+
+function find() {
+  return db("projects");
+}
+
+function findById(id) {
+  return db("projects")
+    .where("project_id", id)
+    .then((data) => {
+      return data[0];
+    });
+}
+
+function insert(project) {
+  return db("projects")
+    .insert(project)
+    .then((ids) => {
+      return findById(ids[0]);
+    });
+}
