184667a312c5954853b2bb877a9787b6c5e77591
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-06-11 11:05:19 -0400

    wrote get request for all projects

--- api/project/router.js
+++ api/project/router.js
@@ -1,6 +1,15 @@
 // build your `/api/projects` router here
 const express = require("express");
 const router = express.Router();
-const db = require("./model");
+const Projects = require("./model");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const project = await Projects.find();
+    res.json(project);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
