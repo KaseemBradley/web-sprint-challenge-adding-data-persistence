6ffdec3f97d9361332257f8d63f7ae1a60a2b188
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-06-11 10:08:41 -0400

    started server on index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // start your server here
+const server = require("./api/server");
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
